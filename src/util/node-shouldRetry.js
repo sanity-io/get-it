1d010f50e0793006353ba9e548d60dcf2e271b59
Espen Hovlandsdal <espen@hovlandsdal.com>
2016-12-12 11:59:15 +0100

    Don't allow retrying HTTP errors by default

--- src/util/node-shouldRetry.js
+++ src/util/node-shouldRetry.js
@@ -1,1 +1,10 @@
-module.exports = require('is-retry-allowed')
+const allowed = require('is-retry-allowed')
+
+module.exports = err => {
+  // Don't allow retries if we get any http status code by default
+  if (err.response && err.response.statusCode) {
+    return false
+  }
+
+  return allowed(err)
+}
