1c2c1a0e4fb9f333a05c5ddd218c317e1576142a
Espen Hovlandsdal <espen@hovlandsdal.com>
2017-11-21 13:28:10 +0100

    Allow promise middleware to only resolve response body (#11)

--- src/middleware/promise.js
+++ src/middleware/promise.js
@@ -3,8 +3,8 @@ const Cancel = require('./cancel/Cancel')
 const CancelToken = require('./cancel/CancelToken')
 const isCancel = require('./cancel/isCancel')
 
-const promise = () => {
-  const Promise = global.Promise
+const promise = (options = {}) => {
+  const Promise = options.implementation || global.Promise
   if (!Promise) {
     throw new Error('`Promise` is not available in global scope, and no implementation was passed')
   }
@@ -20,7 +20,9 @@ const promise = () => {
       }
 
       channels.error.subscribe(reject)
-      channels.response.subscribe(resolve)
+      channels.response.subscribe(response => {
+        resolve(options.onlyBody ? response.body : response)
+      })
 
       // Wait until next tick in case cancel has been performed
       setTimeout(() => channels.request.publish(context), 0)
