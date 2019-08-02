5b4d15692cd9b635874ff09b18ec71ecbf8f226b
Espen Hovlandsdal <espen@hovlandsdal.com>
2019-08-02 15:02:54 +0200

    Truncate long URLs from error messages

--- src/middleware/httpErrors.js
+++ src/middleware/httpErrors.js
@@ -1,7 +1,8 @@
 const createErrorClass = require('create-error-class')
 
 const HttpError = createErrorClass('HttpError', function(res, ctx) {
-  let msg = `${res.method}-request to ${res.url} resulted in`
+  const truncatedUrl = res.url.length > 400 ? `${res.url.slice(0, 399)}…` : res.url
+  let msg = `${res.method}-request to ${truncatedUrl} resulted in `
   msg += `HTTP ${res.statusCode} ${res.statusMessage}`
 
   this.message = msg.trim()
--- test/errors.test.js
+++ test/errors.test.js
@@ -1,14 +1,8 @@
 const {httpErrors} = require('../src/middleware')
 const requester = require('../src/index')
-const {
-  expectRequest,
-  expect,
-  baseUrl,
-  baseUrlPrefix
-} = require('./helpers')
+const {expectRequest, expect, baseUrl, baseUrlPrefix} = require('./helpers')
 
 describe('errors', () => {
-
   it('should not respond with errors on HTTP >= 400 by default', () => {
     const request = requester([baseUrl])
     const req = request({url: '/status?code=400'})
@@ -23,19 +17,42 @@ describe('errors', () => {
     })
     req.error.subscribe(err => {
       expect(err).to.be.an.instanceOf(Error)
-      expect(err.message).to.include('HTTP 400').and.include('Bad Request')
-      expect(err).to.have.property('response').and.containSubset({
-        url: `${baseUrlPrefix}/status?code=400`,
-        method: 'GET',
-        statusCode: 400,
-        statusMessage: 'Bad Request',
-        body: '---',
-      })
+      expect(err.message).to.eq(
+        'GET-request to http://localhost:9980/req-test/status?code=400 resulted in HTTP 400 Bad Request'
+      )
+      expect(err.message)
+        .to.include('HTTP 400')
+        .and.include('Bad Request')
+      expect(err)
+        .to.have.property('response')
+        .and.containSubset({
+          url: `${baseUrlPrefix}/status?code=400`,
+          method: 'GET',
+          statusCode: 400,
+          statusMessage: 'Bad Request',
+          body: '---'
+        })
 
-      expect(err).to.have.deep.property('request.headers').and.containSubset({
-        foo: 'bar'
-      })
+      expect(err)
+        .to.have.deep.property('request.headers')
+        .and.containSubset({
+          foo: 'bar'
+        })
 
+      done()
+    })
+  })
+
+  it('should truncate really long URLs from error message', done => {
+    const request = requester([baseUrl, httpErrors()])
+    const rep = new Array(1024).join('a')
+    const req = request({url: `/status?code=400&foo=${rep}`, headers: {foo: 'bar'}})
+    req.response.subscribe(res => {
+      throw new Error('Response channel called when error channel should have been triggered')
+    })
+    req.error.subscribe(err => {
+      expect(err).to.be.an.instanceOf(Error)
+      expect(err.message).to.have.length.lessThan(600)
       done()
     })
   })
