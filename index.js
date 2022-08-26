6e7cdd451b1918739ee565737d4488bd62d1aba6
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-26 20:59:33 +0200

    fix: add support for vercel and bun

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('./lib-node')
+// If fetch is defined we assume it's either a browser or a worker env (Bun, Deno or Edge) and we divert to the browser version
+// Although Node is shipping `fetch` support soon this check
+// won't affect it as Node respects conditional package exports and this file ain't listed.
+module.exports =
+  typeof fetch === 'function' ? require('./dist/esm/index.mjs') : require('./lib-node')
--- middleware.js
+++ middleware.js
@@ -1,1 +1,7 @@
-module.exports = require('./lib/middleware')
+// If fetch is defined we assume it's either a browser or a worker env (Bun, Deno or Edge) and we divert to the browser version
+// Although Node is shipping `fetch` support soon this check
+// won't affect it as Node respects conditional package exports and this file ain't listed.
+module.exports =
+  typeof fetch === 'function'
+    ? require('./dist/esm/middleware/index.mjs')
+    : require('./lib/middleware')
