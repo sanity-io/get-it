9263d4a044362f9c9f0ff64c9ead6edc71affdf8
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-08-27 02:15:30 +0200

    fix: finally, a verified working edge runtime setup

--- index.module.js
+++
@@ -1,9 +0,0 @@
-// If fetch is defined we assume it's either a browser or a worker env (Bun, Deno or Edge) and we divert to the browser version
-// Although Node is shipping `fetch` support soon this check
-// won't affect it as Node respects conditional package exports and this file ain't listed.
-// The EdgeRuntime check is to support dead-code elimination on Vercel Edge Functions:
-// https://edge-runtime.vercel.sh/features/available-apis#addressing-the-runtime
-module.exports =
-  typeof fetch === 'function' || typeof EdgeRuntime === 'string'
-    ? require('./dist/esm/index.mjs')
-    : require('./lib-node')
