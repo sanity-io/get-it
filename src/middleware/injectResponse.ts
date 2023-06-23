1fc1498464b4a085d3c5feeece69caade35d4e23
Cody Olsen <stipsan@gmail.com>
2023-06-23 14:44:53 +0200

    refactor: improve `injectResponse` typings

--- src/middleware/injectResponse.ts
+++ src/middleware/injectResponse.ts
@@ -1,7 +1,15 @@
-import type {Middleware} from '../types'
+import type {Middleware, MiddlewareHooks, MiddlewareResponse} from '../types'
 
 /** @public */
-export function injectResponse(opts: any = {}) {
+export function injectResponse(
+  opts: {
+    inject: (
+      event: Parameters<MiddlewareHooks['interceptRequest']>[1],
+      prevValue: Parameters<MiddlewareHooks['interceptRequest']>[0]
+    ) => Partial<MiddlewareResponse | undefined | void>
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } = {} as any
+) {
   if (typeof opts.inject !== 'function') {
     throw new Error('`injectResponse` middleware requires a `inject` function')
   }
@@ -14,18 +22,15 @@ export function injectResponse(opts: any = {}) {
 
     // Merge defaults so we don't have to provide the most basic of details unless we want to
     const options = event.context.options
-    return Object.assign(
-      {},
-      {
-        body: '',
-        url: options.url,
-        method: options.method,
-        headers: {},
-        statusCode: 200,
-        statusMessage: 'OK',
-      },
-      response
-    )
+    return {
+      body: '',
+      url: options.url,
+      method: options.method!,
+      headers: {},
+      statusCode: 200,
+      statusMessage: 'OK',
+      ...response,
+    } satisfies MiddlewareResponse
   } satisfies Middleware['interceptRequest']
 
   return {interceptRequest: inject} satisfies Middleware
