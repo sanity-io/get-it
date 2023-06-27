6b98212ba75a85d6e7746167f7ebbb0b939e82f6
Cody Olsen <stipsan@gmail.com>
2023-06-28 00:40:48 +0200

    chore: bust the cache

--- test-next/app/utils.ts
+++ test-next/app/utils.ts
@@ -1,7 +1,7 @@
 export async function getTimestamp(runtime: string) {
   const [dynamicRes, staticRes] = await Promise.all([
     fetch('https://apicdn.sanity.io', {cache: 'no-store'}),
-    fetch('https://apicdn.sanity.io', {next: {tags: [runtime]}}),
+    fetch('https://api.sanity.io', {next: {tags: [runtime]}}),
   ])
   return [dynamicRes.headers.get('date'), staticRes.headers.get('date')]
 }
