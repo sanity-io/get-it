821179ef618876e3a6fd0945a887d3ebe96be802
Cody Olsen <stipsan@gmail.com>
2023-06-28 00:02:58 +0200

    chore: debug revalidate tag

--- test-next/app/edge/page.tsx
+++ test-next/app/edge/page.tsx
@@ -1,8 +1,8 @@
 export const runtime = 'edge'
 
 export default async function EdgePage() {
-  const timestamp = (
-    await fetch('https://apicdn.sanity.io', {next: {tags: [runtime]}})
-  ).headers.get('date')
+  const timestamp = (await fetch('https://apicdn.sanity.io', {next: {tags: ['edge']}})).headers.get(
+    'date'
+  )
   return <p id="edge">{timestamp}</p>
 }
--- test-next/app/nodejs/page.tsx
+++ test-next/app/nodejs/page.tsx
@@ -2,7 +2,7 @@ export const runtime = 'nodejs'
 
 export default async function EdgePage() {
   const timestamp = (
-    await fetch('https://apicdn.sanity.io', {next: {tags: [runtime]}})
+    await fetch('https://apicdn.sanity.io', {next: {tags: ['nodejs']}})
   ).headers.get('date')
   return <p id="nodejs">{timestamp}</p>
 }
