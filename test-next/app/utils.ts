ebd8e03d1ed96b4f83a717d0c8dd1a9e87d45043
Cody Olsen <stipsan@gmail.com>
2023-08-03 18:15:56 +0200

    chore: less strict

--- test-next/app/utils.ts
+++ test-next/app/utils.ts
@@ -11,11 +11,11 @@ export async function getTimestamp(runtime: string) {
     request({
       url: 'https://ppsg7ml5.apicdn.sanity.io/v1/data/query/test?query=now()',
       fetch: {cache: 'no-store'},
-    }).then((json: any) => json.body.result),
+    }).then((json: any) => json.body?.result),
     request({
       url: 'https://ppsg7ml5.api.sanity.io/v1/data/query/test?query=now()',
       fetch: {cache: 'force-cache', next: {tags: [runtime]}},
-    }).then((json: any) => json.body.result),
+    }).then((json: any) => json.body?.result),
   ])
   return [dynamicRes, staticRes]
 }
