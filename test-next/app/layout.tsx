f53455c9d8cf3d9f658caacc7e58c7dcada3b1b5
Cody Olsen <stipsan@gmail.com>
2023-08-06 12:40:42 +0200

    chore: better smoketest

--- test-next/app/layout.tsx
+++ test-next/app/layout.tsx
@@ -1,7 +1,23 @@
-export default function RootLayout({children}: {children: React.ReactNode}) {
+export default async function RootLayout({children}: {children: React.ReactNode}) {
+  // Dedupe test based on https://github.com/vercel/next.js/blob/5f9d2c55ca3ca3bd6a01cf60ced69d3dd2c64bf4/test/e2e/app-dir/app-fetch-deduping/app-fetch-deduping.test.ts#L50-L79
+  if ((await getDuplicate()) !== (await getDuplicate())) {
+    throw new Error('Deduping failed')
+  }
+
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   )
+}
+
+async function getDuplicate() {
+  // Passing a signal to the fetch call is supposed to opt-out of request memoization
+  const {signal} = new AbortController()
+  const res = await fetch('https://ppsg7ml5.api.sanity.io/vX/data/query/test?query=now()', {
+    signal,
+    cache: 'force-cache',
+  })
+  const json = await res.json()
+  return json.result
 }
--- test-next/app/utils.ts
+++ test-next/app/utils.ts
@@ -6,20 +6,7 @@ import {jsonResponse, promise} from 'get-it/middleware'
 
 const request = getIt([jsonResponse(), promise()])
 
-export async function getDuplicate() {
-  // Passing a signal to the fetch call is supposed to opt-out of request memoization
-  const {signal} = new AbortController()
-  const res = await fetch('https://ppsg7ml5.api.sanity.io/vX/data/query/test?query=now()', {signal})
-  const json = await res.json()
-  return json.result
-}
-
 export async function getTimestamp(runtime: string) {
-  // Dedupe test based on https://github.com/vercel/next.js/blob/5f9d2c55ca3ca3bd6a01cf60ced69d3dd2c64bf4/test/e2e/app-dir/app-fetch-deduping/app-fetch-deduping.test.ts#L50-L79
-  if ((await getDuplicate()) !== (await getDuplicate())) {
-    throw new Error('Deduping failed')
-  }
-
   const [dynamicRes, staticRes] = await Promise.all([
     request({
       url: 'https://ppsg7ml5.apicdn.sanity.io/v1/data/query/test?query=now()',
