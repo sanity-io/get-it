ead5ceb1e07eaff9e1f4c2f966aa589f1e22a4c8
Cody Olsen <stipsan@gmail.com>
2023-08-09 21:53:32 +0200

    fix: improve React Native compatiblity

--- src/middleware/defaultOptionsProcessor.ts
+++ src/middleware/defaultOptionsProcessor.ts
@@ -11,8 +11,8 @@ export const processOptions = function processOptions(opts) {
     ...(typeof opts === 'string' ? {url: opts} : opts),
   } satisfies RequestOptions
 
-  // Allow parsing relativ URLs by setting the origin
-  const url = new URL(options.url, 'http://localhost')
+  // Allow parsing relative URLs by setting the origin to `http://localhost`
+  const {searchParams} = new URL(options.url, 'http://localhost')
 
   // Normalize timeouts
   options.timeout = normalizeTimeout(options.timeout)
@@ -23,22 +23,24 @@ export const processOptions = function processOptions(opts) {
       if (value !== undefined) {
         if (Array.isArray(value)) {
           for (const v of value) {
-            url.searchParams.append(key, v as string)
+            searchParams.append(key, v as string)
           }
         } else {
-          url.searchParams.append(key, value as string)
+          searchParams.append(key, value as string)
         }
       }
     }
+  }
+  // Merge back params into url
+  const [url] = options.url.split('?')
+  const search = searchParams.toString()
+  if (search) {
+    options.url = `${url}?${search}`
   }
 
   // Implicit POST if we have not specified a method but have a body
   options.method =
     options.body && !options.method ? 'POST' : (options.method || 'GET').toUpperCase()
-
-  // Stringify URL
-  options.url =
-    url.origin === 'http://localhost' ? `${url.pathname}?${url.searchParams}` : url.toString()
 
   return options
 } satisfies MiddlewareHooks['processOptions']
