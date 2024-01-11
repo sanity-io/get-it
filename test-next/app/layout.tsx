2ecc63ca513a88f14260f225148d6749a2d2e430
Cody Olsen <stipsan@gmail.com>
2024-01-11 13:02:31 +0100

    chore: improve dedupe test

--- test-next/app/layout.tsx
+++ test-next/app/layout.tsx
@@ -1,9 +1,9 @@
 export default async function RootLayout({children}: {children: React.ReactNode}) {
+  const id = `${Date.now()}-${Math.random()}`
   // Dedupe test based on https://github.com/vercel/next.js/blob/5f9d2c55ca3ca3bd6a01cf60ced69d3dd2c64bf4/test/e2e/app-dir/app-fetch-deduping/app-fetch-deduping.test.ts#L50-L79
-  if ((await getDuplicate()) !== (await getDuplicate())) {
+  if ((await getDuplicate(id)) === (await getDuplicate(id))) {
     throw new Error('Deduping failed')
   }
-
   return (
     <html lang="en">
       <body>{children}</body>
@@ -11,13 +11,11 @@ export default async function RootLayout({children}: {children: React.ReactNode}
   )
 }
 
-async function getDuplicate() {
+async function getDuplicate(id: string) {
   // Passing a signal to the fetch call is supposed to opt-out of request memoization
   const {signal} = new AbortController()
-  const res = await fetch('https://ppsg7ml5.api.sanity.io/vX/data/query/test?query=now()', {
+  const res = await fetch(`https://get-it-test-next.sanity.build/api/random?id=${id}`, {
     signal,
-    cache: 'force-cache',
   })
-  const json = await res.json()
-  return json.result
+  return await res.text()
 }
