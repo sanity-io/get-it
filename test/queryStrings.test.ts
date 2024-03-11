b91bfd174459ae011ac12115c5fe95c232e9eaa0
Espen Hovlandsdal <espen@hovlandsdal.com>
2024-03-11 01:47:47 -0700

    fix: query string merging in legacy react native versions

--- test/queryStrings.test.ts
+++ test/queryStrings.test.ts
@@ -36,4 +36,94 @@ describe('query strings', () => {
     const req = request({url: '/query-string', query})
     await expectRequestBody(req).resolves.toEqual({bar: 'baz'})
   })
+
+  it('should handle URLs with duplicate query params', async () => {
+    const request = getIt([baseUrl, jsonResponse()])
+    const req = request({url: '/debug?dupe=1&dupe=2&lone=3'})
+    await expectRequestBody(req).resolves.toHaveProperty(
+      'url',
+      '/req-test/debug?dupe=1&dupe=2&lone=3',
+    )
+  })
+
+  it('should append explicitly passed query parameters with existing params in URL', async () => {
+    const request = getIt([baseUrl, jsonResponse()])
+    const req = request({url: '/debug?dupe=a', query: {dupe: 'b', lone: 'c'}})
+    await expectRequestBody(req).resolves.toHaveProperty(
+      'url',
+      '/req-test/debug?dupe=a&dupe=b&lone=c',
+    )
+  })
+
+  it('should handle query parameter values with escaped `&`, `=`, and `?` (in uri)', async () => {
+    const request = getIt([baseUrl, jsonResponse(), debugRequest])
+    const req = request({
+      url: '/query-string?and=this%26that&equals=this%3Dthat&question=this%3Fthat',
+    })
+    await expectRequestBody(req).resolves.toEqual({
+      and: 'this&that',
+      equals: 'this=that',
+      question: 'this?that',
+    })
+  })
+
+  it('should handle query parameter values with `&`, `=`, and `?` (in `query` option)', async () => {
+    const request = getIt([baseUrl, jsonResponse(), debugRequest])
+    const req = request({
+      url: '/query-string',
+      query: {and: 'this&that', equals: 'this=that', question: 'this?that'},
+    })
+    await expectRequestBody(req).resolves.toEqual({
+      and: 'this&that',
+      equals: 'this=that',
+      question: 'this?that',
+    })
+  })
+
+  it('should handle query parameter values with `&`, `=`, and `?` (mixed)', async () => {
+    const request = getIt([baseUrl, jsonResponse(), debugRequest])
+    const req = request({
+      url: '/query-string?and=this%26that&question=this%3Fthat',
+      query: {equals: 'this=that'},
+    })
+    await expectRequestBody(req).resolves.toEqual({
+      and: 'this&that',
+      equals: 'this=that',
+      question: 'this?that',
+    })
+  })
+
+  it('should handle query parameter values with double equals (uri)', async () => {
+    const request = getIt([baseUrl, jsonResponse(), debugRequest])
+    const req = request({
+      url: '/query-string?query=_type%20%3D%3D+%22test%22',
+    })
+    await expectRequestBody(req).resolves.toEqual({
+      query: '_type == "test"',
+    })
+  })
+
+  it('should handle query parameter values with double equals (uri + query option)', async () => {
+    const request = getIt([baseUrl, jsonResponse(), debugRequest])
+    const req = request({
+      url: '/query-string?query=_type+%3D%3D%20%22test%22',
+      query: {$type: 'itsa == test'},
+    })
+    await expectRequestBody(req).resolves.toEqual({
+      query: '_type == "test"',
+      $type: 'itsa == test',
+    })
+  })
+
+  it('should handle query parameters with empty values', async () => {
+    const request = getIt([baseUrl, jsonResponse(), debugRequest])
+    const req = request({
+      url: '/query-string?a=',
+      query: {b: ''},
+    })
+    await expectRequestBody(req).resolves.toEqual({
+      a: '',
+      b: '',
+    })
+  })
 })
