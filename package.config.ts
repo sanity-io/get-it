23bc133992c0601629ead4f966107ed39d6fc1ca
Cody Olsen <stipsan@gmail.com>
2024-08-09 18:51:32 +0200

    fix: regression where inlining `debug` breaks next.js envs on custom babel

--- package.config.ts
+++ package.config.ts
@@ -8,4 +8,9 @@ export default defineConfig({
       import: './dist/index.react-server.js',
     },
   ],
+  // Setting up Terser here to workaround a issue where Next.js fails to import from a ESM file that has a reference to `module.exports` somewhere in the file:
+  // https://github.com/vercel/next.js/issues/57962
+  // By enabling minification the problematic reference is changed so the issue is avoided.
+  // The reason this happens is because we're inlining the `debug` module, which is CJS only. We can't stop inlining this module as it breaks Hydrogen compatibility.
+  minify: true,
 })
