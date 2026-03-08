17594d5045d574ba2f0e965c7333d389dcfb7115
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-03-08 10:09:18 -0700

    test: exclude node tests in edge/happy-dom/react-server tests

--- vitest.edge.config.ts
+++ vitest.edge.config.ts
@@ -1,10 +1,11 @@
-import {defineConfig} from 'vitest/config'
+import {configDefaults, defineConfig} from 'vitest/config'
 
 import {sharedConfig} from './vitest.config'
 
 export default defineConfig({
   test: {
     ...sharedConfig,
+    exclude: [...configDefaults.exclude, 'test/*.node.test.ts'],
     environment: 'edge-runtime',
   },
 })
--- vitest.happy-dom.config.ts
+++ vitest.happy-dom.config.ts
@@ -1,10 +1,11 @@
-import {defineConfig} from 'vitest/config'
+import {configDefaults, defineConfig} from 'vitest/config'
 
 import {sharedConfig} from './vitest.config'
 
 export default defineConfig({
   test: {
     ...sharedConfig,
+    exclude: [...configDefaults.exclude, 'test/*.node.test.ts'],
     environment: 'happy-dom',
     setupFiles: ['./test/helpers/suppress-noise.ts'],
   },
