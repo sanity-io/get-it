320e50143e03c7dbb8f7238709cefaf9d1198673
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-03-08 08:54:01 -0700

    test: add playwright browser tests

---
+++ vitest.browser.config.ts
@@ -0,0 +1,23 @@
+import {playwright} from '@vitest/browser-playwright'
+import {configDefaults, defineConfig} from 'vitest/config'
+
+import {sharedConfig} from './vitest.config'
+
+const {globalSetup, ...browserSharedConfig} = sharedConfig
+
+export default defineConfig({
+  test: {
+    ...browserSharedConfig,
+    exclude: [...configDefaults.exclude, 'test/*.node.test.ts'],
+    globalSetup,
+    browser: {
+      enabled: true,
+      provider: playwright(),
+      instances: [
+        {browser: 'chromium', headless: true},
+        {browser: 'firefox', headless: true},
+        {browser: 'webkit', headless: true}
+      ],
+    },
+  },
+})
