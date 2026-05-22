974032f6e919ad0017b645d0b9768084448b91ef
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-05-22 15:08:53 -0700

    chore: improve node output

--- package.config.ts
+++ package.config.ts
@@ -4,11 +4,6 @@ export default defineConfig({
   tsconfig: 'tsconfig.dist.json',
   bundles: [
     {
-      source: './src/_exports/index.node.ts',
-      import: './dist/index.node.js',
-      runtime: 'node',
-    },
-    {
       source: './src/_exports/vitest.ts',
       import: './dist/vitest.js',
     },
