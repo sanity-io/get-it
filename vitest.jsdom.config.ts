2f2c0103f0339a28100362a400aff54e80f17863
Espen Hovlandsdal <espen@hovlandsdal.com>
2026-08-17 17:35:01 -0700

    test: add jsdom to test matrix

---
+++ vitest.jsdom.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from 'vitest/config'
+
+import {nonNodeExclude, sharedConfig} from './vitest.config'
+
+export default defineConfig({
+  test: {
+    ...sharedConfig,
+    exclude: nonNodeExclude,
+    environment: 'jsdom',
+  },
+})
